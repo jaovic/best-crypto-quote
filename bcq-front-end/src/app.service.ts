26f93938fc41a5a9cfc83d0f39c97b4b4741cf2f
João Victor Martins <j_victorfernandes@hotmail.com>
2022-09-12 12:38:15 -0300

    ✨ kafka producer crypto

--- bcq-front-end/src/app.service.ts
+++ bcq-front-end/src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { selectCryptoDto } from './dto/crypto.select.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly kafkaService: ClientKafka,
+  ) {}
+  async selectCrypto(crypto: selectCryptoDto) {
+    this.kafkaService.emit('selectCrypto', crypto);
   }
 }

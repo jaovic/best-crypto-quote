5f2fab018c42a7ba792d88ed958759e708109a19
João Victor Martins <j_victorfernandes@hotmail.com>
2022-09-12 15:20:02 -0300

    ♻️ kafka config

--- bcq-processor/src/main.ts
+++ bcq-processor/src/main.ts
@@ -1,15 +1,23 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  );
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+        consumer: {
+          groupId: 'getway-consumer',
+        },
+      },
+    },
+  });
+  await app.startAllMicroservices();
   const PORT = process.env.PORT;
   console.log(`Server running on PORT: ${PORT}`);
-
   await app.listen(PORT);
 }
 bootstrap();

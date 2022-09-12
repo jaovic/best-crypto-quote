515455bd79cca877997ac80a08ffec06de8489a7
João Victor Martins <j_victorfernandes@hotmail.com>
2022-09-12 12:39:03 -0300

    ✨ endpoint crypto

--- bcq-front-end/src/app.controller.ts
+++ bcq-front-end/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { selectCryptoDto } from './dto/crypto.select.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('crypto')
+  selectCrypto(crypto: selectCryptoDto) {
+    return this.appService.selectCrypto(crypto);
   }
 }

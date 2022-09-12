1d3c64aad331c69df25d841a39298338b8b6c8c9
João Victor Martins <j_victorfernandes@hotmail.com>
2022-09-12 12:38:42 -0300

    ✨ kafka crypto consumer

---
+++ bcq-processor/src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller } from '@nestjs/common';
+import { AppService } from './app.service';
+import { selectCryptoDto } from './dto/crypto.select.dto';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @EventPattern('selectcrypto')
+  selectCrypto(@Payload() message: selectCryptoDto) {
+    return this.appService.selectCrypto(message);
+  }
+}

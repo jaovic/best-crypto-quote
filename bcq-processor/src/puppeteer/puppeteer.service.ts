45b555b6fba985946904f585683dd26affc3a9f0
João Victor Martins <j_victorfernandes@hotmail.com>
2022-09-12 15:56:50 -0300

    ✨ puppeteer logic

--- bcq-processor/src/puppeteer/puppeteer.service.ts
+++ bcq-processor/src/puppeteer/puppeteer.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import puppeteer from 'puppeteer';
 
 @Injectable()
 export class PuppeteerService {
   async bitcoin(crypto: string) {
     console.log(crypto);
     console.log('Bitcoin Selecionado Puppeteer');
+
+    const browser = await puppeteer.launch({
+      headless: true,
+      executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+    });
+    // cria uma nova pagina
+    const page = await browser.newPage();
+    // entra na url
+    await page.goto('google.com');
   }
   async ethereum(crypto: string) {
     console.log(crypto);
